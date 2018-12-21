a32260f9b4e70ac7c593c20a844f84947cc7075f
drzz <skyofsmith@163.com>
2018-12-21 22:56:46 +0800

    add many param

--- vuex/x1/src/App.vue
+++ vuex/x1/src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    {{count}}
+    {{c}}
     <button @click="add">Add</button>
     <button @click="dec">Dec</button>
+    <hr>
+    {{n}}
+    <input type="text" v-model="name" @change="nameChange">
+    {{a}}
+    <input type="text" v-model.number="age" @change="ageChange">
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data () {
+    return {
+      name: '',
+      age: 0
+    }
+  },
+  created () {
+    this.name = this.$store.state.name
+    this.age = this.$store.state.age
+  },
   computed: {
-    count () {
+    c () {
       return this.$store.state.count
+    },
+    n () {
+      return this.$store.state.name
+    },
+    a () {
+      return this.$store.state.age
     }
   },
   methods: {
@@ -20,6 +41,14 @@ export default {
     },
     dec () {
       this.$store.commit('decrement')
+    },
+    nameChange () {
+      window.console.log('name changed')
+      this.$store.commit('setName', this.name)
+    },
+    ageChange () {
+      window.console.log('name changed')
+      this.$store.commit('setAge', this.age)
     }
   },
   components: {
--- vuex/x1/src/store.js
+++ vuex/x1/src/store.js
@@ -5,7 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    name: 'sam',
+    age: 27
   },
   mutations: {
     increment (state) {
@@ -13,6 +15,12 @@ export default new Vuex.Store({
     },
     decrement (state) {
       state.count--
+    },
+    setName (state, newName) {
+      state.name = newName
+    },
+    setAge (state, newAge) {
+      state.age = newAge
     }
   },
   actions: {
