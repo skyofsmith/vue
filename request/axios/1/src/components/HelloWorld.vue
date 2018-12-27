d15dfa7dc5425745ab4bb2923299a5aa487185ef
drzz <skyofsmith@163.com>
2018-12-27 20:55:13 +0800

    begin axios learn

---
+++ request/axios/1/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+window.console.log(axios);
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted () {
+    
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
