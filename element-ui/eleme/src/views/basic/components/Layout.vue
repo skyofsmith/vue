ffe04bb3daeeace092d59b689dad20fe5f47cb02
drzz <skyofsmith@163.com>
2019-01-17 23:37:23 +0800

    add grid

--- element-ui/eleme/src/views/basic/components/Layout.vue
+++ element-ui/eleme/src/views/basic/components/Layout.vue
@@ -1,94 +1,234 @@
-<style lang="postcss" scoped>
-.base {
-  .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+<style lang="postcss">
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
   }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+  line-height: 36px;
+  font-size: 24px;
+  text-align: center;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
 }
 </style>
 
 <template>
   <div>
     <div>layout</div>
+    <h4>base</h4>
     <div class="base">
       <el-row>
         <el-col :span="24">
-          <div class="grid-content bg-purple-dark"></div>
+          <div class="grid-content bg-purple-dark">24</div>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="12">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">12</div>
         </el-col>
         <el-col :span="12">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">12</div>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="8">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">8</div>
         </el-col>
         <el-col :span="8">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">8</div>
         </el-col>
         <el-col :span="8">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">8</div>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="6">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">6</div>
         </el-col>
         <el-col :span="6">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">6</div>
         </el-col>
         <el-col :span="6">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">6</div>
         </el-col>
         <el-col :span="6">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">6</div>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="4">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">4</div>
         </el-col>
         <el-col :span="4">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">4</div>
         </el-col>
         <el-col :span="4">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">4</div>
         </el-col>
         <el-col :span="4">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">4</div>
         </el-col>
         <el-col :span="4">
-          <div class="grid-content bg-purple"></div>
+          <div class="grid-content bg-purple">4</div>
         </el-col>
         <el-col :span="4">
-          <div class="grid-content bg-purple-light"></div>
+          <div class="grid-content bg-purple-light">4</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">3</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple-light">3</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">3</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple-light">3</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">3</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple-light">3</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">3</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple-light">3</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple-light">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple-light">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple-light">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple-light">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple-light">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">2</div>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple-light">2</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple">1</div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple-light">1</div>
         </el-col>
       </el-row>
     </div>
