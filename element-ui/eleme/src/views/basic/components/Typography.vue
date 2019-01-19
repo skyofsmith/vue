419e33d84a30cfeddefbed491973e5ef96734016
drzz <skyofsmith@163.com>
2019-01-19 14:17:05 +0800

    Font

--- element-ui/eleme/src/views/basic/components/Typography.vue
+++ element-ui/eleme/src/views/basic/components/Typography.vue
@@ -1,8 +1,82 @@
 <style scoped>
+.ft {
+  font-size: 30px;
+  margin-left: 30px;
+}
+.ft1 {
+  font-family: "PingFang SC";
+}
+.ft2 {
+  font-family: "Hiragino Sans GB";
+}
+.ft3 {
+  font-family: "Microsoft YaHei";
+}
+.ft4 {
+  font-family: "PingFang SC";
+}
+.ft5 {
+  font-family: "Hiragino Sans GB";
+}
+.ft6 {
+  font-family: "Microsoft YaHei";
+}
+.rule {
+  line-height: 2em;
+}
+.rule1 {
+  font-size: 20px;
+}
+.rule2 {
+  font-size: 18px;
+}
+.rule3 {
+  font-size: 16px;
+}
+.rule4 {
+  font-size: 14px;
+}
+.rule5 {
+  font-size: 13px;
+}
+.rule6 {
+  font-size: 12px;
+}
 </style>
 
 <template>
-  <div>typography</div>
+  <div>
+    <h1>typography</h1>
+    <h2>中文字体</h2>
+    <div class="ft ft1">和畅惠风
+      <br>PingFang SC
+    </div>
+    <div class="ft ft2">和畅惠风
+      <br>Hiragino Sans GB
+    </div>
+    <div class="ft ft3">和畅惠风
+      <br>Microsoft YaHei
+    </div>
+    <h2>中文字体</h2>
+    <div class="ft ft4">RGag
+      <br>Helvetica Neue
+    </div>
+    <div class="ft ft5">RGag
+      <br>Helvetica
+    </div>
+    <div class="ft ft6">RGag
+      <br>Arial
+    </div>
+    <h2>rules</h2>
+    <div class="rule">
+      <div class="rule rule1">主标题 用 Element 快速搭建页面 20px Extra large</div>
+      <div class="rule rule2">标题 用 Element 快速搭建页面 18px large</div>
+      <div class="rule rule3">小标题 用 Element 快速搭建页面 16px Medium</div>
+      <div class="rule rule4">正文 用 Element 快速搭建页面 14px Small</div>
+      <div class="rule rule5">正文（小） 用 Element 快速搭建页面 13px Extra Small</div>
+      <div class="rule rule6">辅助文字 用 Element 快速搭建页面 12px Extra Extra Small</div>
+    </div>
+  </div>
 </template>
 
 <script>
