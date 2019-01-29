2088879abd8b10af008f927fa6615c46cac22bac
drzz <skyofsmith@163.com>
2019-01-29 21:44:58 +0800

    form others

---
+++ element-ui/eleme/src/views/form/components/ColorPicker.vue
@@ -0,0 +1,24 @@
+<style lang="postcss">
+.el-select .el-input {
+  width: 130px;
+}
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+}
+.el-input {
+  width: 200px;
+}
+</style>
+<template>
+  <div class="input"></div>
+</template>
+<script>
+export default {
+  name: 'Input',
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {}
+}
+</script>
