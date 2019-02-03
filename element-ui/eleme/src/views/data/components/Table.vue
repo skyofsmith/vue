b32572f96a9711764d2a6748af008e638fc5fcfd
drzz <skyofsmith@163.com>
2019-02-03 20:01:57 +0800

    Table2

--- element-ui/eleme/src/views/data/components/Table.vue
+++ element-ui/eleme/src/views/data/components/Table.vue
@@ -132,6 +132,100 @@
         <el-button @click="setCurrent()">取消选择</el-button>
       </div>
     </div>
+    <h4>multi select</h4>
+    <div class="multiselect">
+      <el-table
+        ref="multipleTable"
+        :data="tableData3"
+        tooltip-effect="dark"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column label="日期" width="120">
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+      </el-table>
+      <div style="margin-top: 20px">
+        <el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button>
+        <el-button @click="toggleSelection()">取消选择</el-button>
+      </div>
+    </div>
+    <h4>sort</h4>
+    <div class="sort">
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        :default-sort="{prop: 'date', order: 'descending'}"
+      >
+        <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" sortable width="180"></el-table-column>
+        <el-table-column prop="address" label="地址" :formatter="formatter"></el-table-column>
+      </el-table>
+    </div>
+    <h4>filter</h4>
+    <div class="filter">
+      <el-button @click="resetDateFilter">清除日期过滤器</el-button>
+      <el-button @click="clearFilter">清除所有过滤器</el-button>
+      <el-table ref="filterTable" :data="tableData" style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          sortable
+          width="180"
+          column-key="date"
+          :filters="[{text: '2016-05-01', value: '2016-05-01'}, {text: '2016-05-02', value: '2016-05-02'}, {text: '2016-05-03', value: '2016-05-03'}, {text: '2016-05-04', value: '2016-05-04'}]"
+          :filter-method="filterHandler"
+        ></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址" :formatter="formatter"></el-table-column>
+        <el-table-column
+          prop="tag"
+          label="标签"
+          width="100"
+          :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+          :filter-method="filterTag"
+          filter-placement="bottom-end"
+        >
+          <template slot-scope="scope">
+            <el-tag
+              :type="scope.row.tag === '家' ? 'primary' : 'success'"
+              disable-transitions
+            >{{scope.row.tag}}</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <h4>custom col template</h4>
+    <div class="custom-col-template">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column label="日期" width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="姓名" width="180">
+          <template slot-scope="scope">
+            <el-popover trigger="hover" placement="top">
+              <p>姓名: {{ scope.row.name }}</p>
+              <p>住址: {{ scope.row.address }}</p>
+              <div slot="reference" class="name-wrapper">
+                <el-tag size="medium">{{ scope.row.name }}</el-tag>
+              </div>
+            </el-popover>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -393,6 +487,43 @@ export default {
     },
     handleCurrentChange(val) {
       this.currentRow = val
+    },
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row)
+        })
+      } else {
+        this.$refs.multipleTable.clearSelection()
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    },
+    formatter(row, column) {
+      return row.address
+    },
+    resetDateFilter() {
+      this.$refs.filterTable.clearFilter('date')
+    },
+    clearFilter() {
+      this.$refs.filterTable.clearFilter()
+    },
+    formatter(row, column) {
+      return row.address
+    },
+    filterTag(value, row) {
+      return row.tag === value
+    },
+    filterHandler(value, row, column) {
+      const property = column['property']
+      return row[property] === value
+    },
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
     }
   }
 }
