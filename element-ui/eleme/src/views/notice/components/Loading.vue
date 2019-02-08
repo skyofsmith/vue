f355ead14cb2c8997b7ad089e498100f68c9bbe6
drzz <skyofsmith@163.com>
2019-02-08 20:54:34 +0800

    Loading

---
+++ element-ui/eleme/src/views/notice/components/Loading.vue
@@ -0,0 +1,20 @@
+<style>
+</style>
+
+<template>
+  <div class="loading">
+    <h1>loading</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  data() {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
