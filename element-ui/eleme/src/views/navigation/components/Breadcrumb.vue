9c51ab0485ad25c2d5dac93a841d3b77f215ec8f
drzz <skyofsmith@163.com>
2019-02-13 22:21:57 +0800

    Breadcrumb

---
+++ element-ui/eleme/src/views/navigation/components/Breadcrumb.vue
@@ -0,0 +1,35 @@
+<style>
+</style>
+<template>
+  <div class="breadcrumb">
+    <h1>breadcrumb</h1>
+    <h4>basic</h4>
+    <div class="basic">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <a href="/">活动管理</a>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <h4>arrow right</h4>
+    <div class="arrow-right">
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Breadcrumb',
+  data() {
+    return {}
+  }
+}
+</script>
