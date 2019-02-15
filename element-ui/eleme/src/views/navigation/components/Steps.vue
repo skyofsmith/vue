d126d1e831bd2ded5d4f58390d28d0f0b6425f78
drzz <skyofsmith@163.com>
2019-02-15 22:26:06 +0800

    Steps

---
+++ element-ui/eleme/src/views/navigation/components/Steps.vue
@@ -0,0 +1,86 @@
+<style lang="postcss">
+</style>
+<template>
+  <div class="steps">
+    <h1>steps</h1>
+    <h4>basic</h4>
+    <div class="basic">
+      <el-steps :active="active" finish-status="success">
+        <el-step title="步骤 1"></el-step>
+        <el-step title="步骤 2"></el-step>
+        <el-step title="步骤 3"></el-step>
+      </el-steps>
+    </div>
+    <h4>with status</h4>
+    <div class="with-status">
+      <el-steps :space="200" :active="1" finish-status="success">
+        <el-step title="已完成"></el-step>
+        <el-step title="进行中"></el-step>
+        <el-step title="步骤 3"></el-step>
+      </el-steps>
+    </div>
+    <h4>with desc</h4>
+    <div class="with-desc">
+      <el-steps :active="1">
+        <el-step title="步骤 1" description="这是一段很长很长很长的描述性文字"></el-step>
+        <el-step title="步骤 2" description="这是一段很长很长很长的描述性文字"></el-step>
+        <el-step title="步骤 3" description="这段就没那么长了"></el-step>
+      </el-steps>
+    </div>
+    <h4>align-center</h4>
+    <div class="align-center">
+      <el-steps :active="2" align-center>
+        <el-step title="步骤1" description="这是一段很长很长很长的描述性文字"></el-step>
+        <el-step title="步骤2" description="这是一段很长很长很长的描述性文字"></el-step>
+        <el-step title="步骤3" description="这是一段很长很长很长的描述性文字"></el-step>
+        <el-step title="步骤4" description="这是一段很长很长很长的描述性文字"></el-step>
+      </el-steps>
+    </div>
+    <h4>with icon</h4>
+    <div class="with-icon">
+      <el-steps :active="1">
+        <el-step title="步骤 1" icon="el-icon-edit"></el-step>
+        <el-step title="步骤 2" icon="el-icon-upload"></el-step>
+        <el-step title="步骤 3" icon="el-icon-picture"></el-step>
+      </el-steps>
+    </div>
+    <h4>vertical</h4>
+    <div class="vertical">
+      <div style="height: 300px;">
+        <el-steps direction="vertical" :active="1">
+          <el-step title="步骤 1"></el-step>
+          <el-step title="步骤 2"></el-step>
+          <el-step title="步骤 3" description="这是一段很长很长很长的描述性文字"></el-step>
+        </el-steps>
+      </div>
+    </div>
+    <h4>simple</h4>
+    <div class="simpl">
+      <el-steps :active="1" simple>
+        <el-step title="步骤 1" icon="el-icon-edit"></el-step>
+        <el-step title="步骤 2" icon="el-icon-upload"></el-step>
+        <el-step title="步骤 3" icon="el-icon-picture"></el-step>
+      </el-steps>
+      <el-steps :active="1" finish-status="success" simple style="margin-top: 20px">
+        <el-step title="步骤 1"></el-step>
+        <el-step title="步骤 2"></el-step>
+        <el-step title="步骤 3"></el-step>
+      </el-steps>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Steps',
+  data() {
+    return {
+      active: 0
+    }
+  },
+  methods: {
+    next() {
+      if (this.active++ > 2) this.active = 0
+    }
+  }
+}
+</script>
