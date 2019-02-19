7802636aafa145d7a460a34d54c14fa7ad871bf1
drzz <skyofsmith@163.com>
2019-02-19 22:19:40 +0800

    testMultiSPA

---
+++ testMultiSPA/src/admin/App.vue
@@ -0,0 +1,65 @@
+<style>
+html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+.header {
+  height: 80px;
+  background-color: #379cf8;
+}
+.section {
+  flex: 1;
+  background-color: #ccc;
+  display: flex;
+  flex-direction: row;
+  align-self: stretch;
+}
+
+.nav {
+  width: 200px;
+  align-self: stretch;
+}
+
+.content {
+  flex: 1;
+}
+</style>
+
+<template>
+  <div id="app">
+    <div class="header"></div>
+    <div class="section">
+      <Nav class="nav"></Nav>
+      <router-view class="content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from '@/views/Nav'
+export default {
+  name: 'App',
+  components: {
+    Nav
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
---
+++ testMultiSPA/src/admin/views/a/A.vue
@@ -0,0 +1,13 @@
+<style scoped>
+
+</style>
+<template>
+  <div>
+    A
+  </div>
+</template>
+<script>
+export default {
+  name: 'A'
+}
+</script>
