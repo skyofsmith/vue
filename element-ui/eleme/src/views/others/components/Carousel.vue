a7b5b292cfa0df6809cb5d31eb0a0b4b74d8a274
drzz <skyofsmith@163.com>
2019-02-20 20:06:43 +0800

    Carousel

---
+++ element-ui/eleme/src/views/others/components/Carousel.vue
@@ -0,0 +1,73 @@
+<style lang="postcss">
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
+<template>
+  <div class="carousel">
+    <h1>carousel</h1>
+    <h4>basic</h4>
+    <div class="basi">
+      <div class="block">
+        <span class="demonstration">默认 Hover 指示器触发</span>
+        <el-carousel height="150px">
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3>{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div class="block">
+        <span class="demonstration">Click 指示器触发</span>
+        <el-carousel trigger="click" height="150px">
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3>{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
+    <h4>indicator outside</h4>
+    <div class="indicator-outside">
+      <el-carousel indicator-position="outside">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <h4>arrow always show</h4>
+    <div class="arrow-always">
+      <el-carousel :interval="5000" arrow="always">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <h4>card like</h4>
+    <div class="card-like">
+      <el-carousel :interval="4000" type="card" height="200px">
+        <el-carousel-item v-for="item in 6" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Carousel',
+  data() {
+    return {}
+  }
+}
+</script>
