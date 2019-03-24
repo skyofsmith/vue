3e267dac2471334e2612ae545289f337a684b4ad
drzz <skyofsmith@163.com>
2019-03-24 23:25:41 +0800

    materialize-css/

---
+++ materialize-css/src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import 'materialize-css/dist/js/materialize.js'
+import 'materialize-css/dist/css/materialize.css'
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
