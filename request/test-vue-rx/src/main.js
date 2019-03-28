83b2363a9598ea280fbbc471f274ae2e7b71dc8e
drzz <skyofsmith@163.com>
2019-03-28 23:31:28 +0800

    add Observable.

--- request/test-vue-rx/src/main.js
+++ request/test-vue-rx/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 
 import VueRx from "vue-rx";
+import { Observable } from "rxjs";
 // or with script in html file <script src="https://unpkg.com/rxjs/bundles/rxjs.umd.js"></script>
 Vue.use(VueRx);
 
@@ -12,5 +13,8 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  subscriptions: {
+    msg: new Observable()
+  },
   render: h => h(App)
 }).$mount("#app");
