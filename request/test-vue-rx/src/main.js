701f0999a6d2997066b84f5aa7e6f08b85906721
drzz <skyofsmith@163.com>
2019-03-29 23:31:50 +0800

    try rxjs.

--- request/test-vue-rx/src/main.js
+++ request/test-vue-rx/src/main.js
@@ -5,6 +5,8 @@ import store from "./store";
 
 import VueRx from "vue-rx";
 import { Observable } from "rxjs";
+
+import {} from "./rxjs";
 // or with script in html file <script src="https://unpkg.com/rxjs/bundles/rxjs.umd.js"></script>
 Vue.use(VueRx);
 
