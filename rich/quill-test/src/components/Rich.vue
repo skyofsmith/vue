bb09a0e2d64f33d29a0839607828e02801f45397
skyofsmith <skyofsmith@163.com>
2020-06-19 23:26:32 +0800

    quill rich editor

--- rich/quill-test/src/components/Rich.vue
+++ rich/quill-test/src/components/Rich.vue
@@ -22,7 +22,36 @@ export default {
   data() {
     return {
       content: `<p>hello world</p>`,
-      editorOption: {}
+      editorOption: {
+        modules:{
+          toolbar:[
+            ['bold', 'italic', 'underline', 'strike'], //加粗，斜体，下划线，删除线
+            ['blockquote', 'code-block'],  //引用，代码块
+
+
+            [{ 'header': 1 }, { 'header': 2 }],  // 标题，键值对的形式；1、2表示字体大小
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],  //列表
+            [{ 'script': 'sub'}, { 'script': 'super' }], // 上下标
+            [{ 'indent': '-1'}, { 'indent': '+1' }],  // 缩进
+            [{ 'direction': 'rtl' }],    // 文本方向
+
+            [{ 'size': ['small', false, 'large', 'huge'] }], // 字体大小
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],  //几级标题
+
+
+            [{ 'color': [] }, { 'background': [] }],  // 字体颜色，字体背景颜色
+            [{ 'font': [] }],  //字体
+            [{ 'align': [] }], //对齐方式
+
+
+            ['clean'], //清除字体样式
+            ['image','video'] //上传图片、上传视频
+          ],
+          imageDrop:true,
+          imageResize:{ }
+        },
+        theme: 'snow'
+      }
     }
   },
   computed: {
@@ -34,13 +63,13 @@ export default {
     onEditorReady(editor) {
       console.log(editor)
     },
-    onEditorBlur(){// 失去焦点事件
+    onEditorBlur(){ // 失去焦点事件
       console.log('失去焦点事件')
     },
-    onEditorFocus(){// 获得焦点事件
+    onEditorFocus(){ // 获得焦点事件
       console.log('获得焦点事件')
     },
-    onEditorChange(){// 内容改变事件
+    onEditorChange(){ // 内容改变事件
       console.log('内容改变事件')
     },
     saveHtml() {
--- rich/quill-test/src/main.js
+++ rich/quill-test/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import  VueQuillEditor from 'vue-quill-editor'
+import * as Quill from 'quill' //引入编辑器
+import { ImageDrop } from 'quill-image-drop-module';
+
+Quill.register('modules/imageDrop', ImageDrop);
 
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
