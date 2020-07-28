a073b9da156d15234c7399a3b6451d9b149a98df
skyofsmith <skyofsmith@163.com>
2020-07-28 17:55:22 +0800

    v3 learn components communicate

---
+++ v3/demo1/src/components/Child.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="text" v-model="state.msg" >
+    <button @click="clickEvent">emit</button>
+  </div>
+</template>
+
+<script>
+import { reactive
+// , computed, watchEffect, onMounted 
+} from 'vue'
+
+export default {
+  name: 'Child',
+  emits: {
+    'custom-event': ({msg}) => {
+      return msg && msg.length
+    }
+  },
+  setup() {
+    function clickEvent () {
+      this.$emit('custom-event', { msg: state.msg })
+    }
+    const state = reactive({
+      msg: ''
+    })
+    return {
+      state,
+      clickEvent
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- v3/demo1/src/components/HelloWorld.vue
+++ v3/demo1/src/components/HelloWorld.vue
@@ -1,26 +1,47 @@
 <template>
   <div class="hello">
-    <button @click="add">+</button>
-    {{ state.count }}
-    <br>
-    {{ state.double }}
+    <div class="test">
+      <button @click="add">+</button>
+      {{ state.count }}
+      <br>
+      {{ state.double }}
+    </div>
+    <div class="dynamic-attr">
+      <input type="text" @[state.eventName]="func">
+      <select v-model="state.eventName">
+        <option :value="o.value" v-for="o in eventOptions" :key="o.value">{{ o.label }}</option>
+      </select>
+    </div>
+    <div class="child-comp">
+      <child @custom-event="handleCustomEvent" />
+    </div>
   </div>
 </template>
 
 <script>
 import { reactive, computed, watchEffect, onMounted } from 'vue'
+import Child from './Child'
 export default {
   name: 'HelloWorld',
+  components: {
+    Child
+  },
   setup() {
     const state = reactive({
+      eventName: 'click',
       count: 0,
       double: computed(() => state.count * 2)
     });
     function add() {
       state.count++
     }
+    function func() {
+      console.log(`${state.eventName} event triggered!`)
+    }
+    function handleCustomEvent({ msg }) {
+      console.log('HelloWorld received! it\'s ' + msg);
+    }
     watchEffect(() => {
-      debugger
       document.title = `count is ${state.count}`
     });
 
@@ -29,7 +50,28 @@ export default {
     });
 
     return {
-      state, add
+      state,
+      add,
+      func,
+      handleCustomEvent,
+      eventOptions: [
+        {
+          label: 'click',
+          value: 'click'
+        },
+        {
+          label: 'focus',
+          value: 'focus'
+        },
+        {
+          label: 'blur',
+          value: 'blur'
+        },
+        {
+          label: 'doubleclick',
+          value: 'dblclick'
+        }
+      ],
     }
   }
 }
