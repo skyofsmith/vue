73a287ab410963811a19bc408fa60d955c7c40e8
skyofsmith <skyofsmith@163.com>
2020-08-03 14:07:13 +0800

    v3 destructing reactive

--- v3/demo1/src/App.vue
+++ v3/demo1/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr>
+    <hr>
+    <hr>
+    <h1>DestructingReactive</h1>
+    <DestructingReactive />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import DestructingReactive from './components/DestructingReactive'
 export default {
   name: 'App',
   components: {
+    DestructingReactive,
     HelloWorld
   }
 }
---
+++ v3/demo1/src/components/DestructingReactive.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="destructing-reactive">
+    <input type="text" v-model="nameSam" >
+    {{ nameSam }}
+    <input type="checkbox" v-model="genderSam" >
+    {{ genderSam }}
+    <br>
+    <input type="text" v-model="nameTom" >
+    {{ nameTom }}
+    <input type="checkbox" v-model="genderTom" >
+    {{ genderTom }}
+    <br>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, watchEffect } from 'vue'
+export default {
+  name: 'DestructingReactive',
+  setup() {
+    const person = reactive({
+      name: 'zachary'
+    })
+
+    const { name } = person
+
+    watchEffect(() => {
+      console.log(name);
+    })
+    
+    watchEffect(() => {
+      console.log(person.name);
+    })
+
+    const gender = ref(false)
+
+    return {
+      nameSam: person.name,
+      genderSam: gender,
+      genderTom: gender,
+      nameTom: person.name
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .destructing-reactive {
+
+  }
+</style>
